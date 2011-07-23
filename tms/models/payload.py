27e191be391725ada1ff790bf37ff707c6d4f220
barberj <barber.justin@gmail.com>
2011-07-23 17:28:11 -0400

    udpate __str__, added notes on attributes that i need to fill in later

--- tms/models/payload.py
+++ tms/models/payload.py
@@ -1,5 +1,11 @@
-from google.appengine.ext import db
+# payload.py
+"""
+Payload model
+"""
+
 import logging
+
+from google.appengine.ext import db
 
 class Payload(db.Model):
     """
@@ -9,9 +15,13 @@ class Payload(db.Model):
     address = db.StringProperty()
     latitude = db.FloatProperty()
     longitude = db.FloatProperty()
+    # need references to the owner and operator 
+    # and probably some time stamps
 
     def __str__(self):
         """
         Return string representation for Payload object
         """
-        return 'Payload[%s] Latitude: %s, Longitude %s' % ( self.key().id(), self.latitude, self.longitude )
+        return 'Payload[%s] Latitude: %s, Longitude %s' % ( self.key().id(), 
+                                                            self.latitude, 
+                                                            self.longitude )
