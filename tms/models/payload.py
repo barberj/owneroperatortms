1b044678a6eedc069dbcea9032dc00d5df157b30
barberj <barber.justin@gmail.com>
2011-07-25 04:40:02 -0400

    added instance variables: delivery addr and tmstamps

--- tms/models/payload.py
+++ tms/models/payload.py
@@ -1,6 +1,7 @@
 # payload.py
 """
 Payload model
+- the load carried by a vehicle exclusive of what is necessary for its operation
 """
 
 import logging
@@ -12,11 +13,17 @@ class Payload(db.Model):
     Payload model. Will keep track of description, location, reference to owner.
     """
 
-    address = db.StringProperty()
+    pickup_address = db.StringProperty()
+    delivery_address = db.StringProperty()
+
+    created_at = db.DateTimeProperty()
+    updated_at = db.DateTimeProperty()
+    delivered_at = db.DateTimeProperty()
+
     latitude = db.FloatProperty()
     longitude = db.FloatProperty()
     # need references to the owner and operator 
-    # and probably some time stamps
+    # and users who manipulate
 
     def __str__(self):
         """
