d818e077f93865a69ebd76038dfcac80f41382c7
barberj <barber.justin@gmail.com>
2011-08-28 19:56:18 -0400

    terminology update... The org that manages is a broker... not owner. Owner/Operator really represents the 'Transporter' class

--- www/tms/models/broker.py
+++ www/tms/models/broker.py
@@ -1,17 +1,19 @@
 # broker.py
 """
 Broker model
+- the organization or individual who creates and manages deliveries
 - transporter firm that acts as an agent for a shipper to commission
     a transporter
 """
-
-import logging
 
 from google.appengine.ext import db
 
 class Broker(db.Model):
     """
     Broker model.
+    
+    Organization which schedules deliveries and is in need of transport
+    Associated to many users who are able to admin over the deliveries
     """
 
     created_at = db.DateTimeProperty(auto_now_add=True)
