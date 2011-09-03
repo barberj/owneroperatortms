f2339984441a38d96e5d6120466b0c16c8cd145e
barberj <barber.justin@gmail.com>
2011-09-03 14:49:23 -0400

    abstracting addresses to the current model object versus a sting. Model object will include helpers for converting to lat/long i presume

--- www/tms/models/payload.py
+++ www/tms/models/payload.py
@@ -11,6 +11,7 @@ from google.appengine.ext import db
 from trackable import Trackable
 from broker import Broker
 from transporter import Transporter
+from contact import Address
 
 class PlannedPayload(Trackable):
     """
@@ -37,19 +38,22 @@ class Payload(Trackable):
     A confirmed payload requiring a pickup and delivery on a schedule.
     """
 
-    pickup_address = db.StringProperty()
-    delivery_address = db.StringProperty()
+    pickup_address = db.ReferenceProperty(Address, collection_name='pickedup_payloads')
+    delivery_address = db.ReferenceProperty(Address, collection_name='delivered_payloads')
 
     pickedup_at = db.DateTimeProperty()
     delivered_at = db.DateTimeProperty()
 
-    # need references to the owner and operator
+    # need references to the broker and transporter
     # and users who manipulate
-    transporter = db.ReferenceProperty(Transporter,
-                                       collection_name='payloads')
-
     broker = db.ReferenceProperty(Broker,
                                     collection_name='payloads')
 
+    transporter = db.ReferenceProperty(Transporter,
+                                       collection_name='payloads')
+
     def __str__(self):
         return 'Payload%s' % super(PlannedPayload,self).__str__()
+
+    def get_nearby_transporters(self):
+        pass
